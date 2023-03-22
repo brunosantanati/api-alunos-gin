db1f715660ed341607319179e3503f1a7cbfad4b
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-22 11:52:03 -0300

    Estilizar a pagina com CSS

--- routes/routes.go
+++ routes/routes.go
@@ -8,6 +8,7 @@ import (
 func HandleRequests() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/:nome", controllers.Saudacao)
 	r.POST("/alunos", controllers.CriaNovoAluno)
